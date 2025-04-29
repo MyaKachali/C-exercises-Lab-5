a2115b7fad0887a97c528a6c351a4ae38ad9c330
MyaKachali <bsc-com-27-22@unima.ac.mw>
2025-04-29 03:59:44 +0200

    Update SubscriptionChecker.cpp

--- SubscriptionChecker.cpp
+++ SubscriptionChecker.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
-#include <cstdlib>  // for rand and srand
-#include <ctime>    // for time (used in seeding)
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
+
 using namespace std;
 
 int main() {
-    srand(time(0)); // Seed the random number generator with the current time
+    // Seed the random number generator
+    srand(time(0));
 
-    int daysUntilExpiration = rand() % 12; // Random number between 0 and 11
-    cout << "Days until expiration: " << daysUntilExpiration << endl;
+    // Generate random number between 0 and 11
+    int daysUntilExpiration = rand() % 12;
 
-    if (daysUntilExpiration == 0) {
-        // If subscription has expired
-        cout << "Your subscription has expired." << endl;
+    // Output the appropriate message based on conditions
+    if (daysUntilExpiration <= 10) {
+        if (daysUntilExpiration <= 5) {
+            if (daysUntilExpiration == 1) {
+                cout << "Your subscription expires within a day!\n"
+                     << "Renew now and save 20%!" << endl;
+            }
+            else if (daysUntilExpiration == 0) {
+                cout << "Your subscription has expired." << endl;
+            }
+            else {
+                cout << "Your subscription expires in " << daysUntilExpiration << " days\n"
+                     << "Renew now and save 10%!" << endl;
+            }
+        }
+        else {
+            cout << "Your subscription will expire soon. Renew now!" << endl;
+        }
     }
-
-    if (daysUntilExpiration == 1) {
-        // If 1 day left
-        cout << "Your subscription expires within a day!\nRenew now and save 20%!" << endl;
-    }
-
-    if (daysUntilExpiration <= 5 && daysUntilExpiration > 1) {
-        // If between 2 and 5 days left
-        cout << "Your subscription expires in " << daysUntilExpiration << " days.\nRenew now and save 10%!" << endl;
-    }
-
-    if (daysUntilExpiration <= 10 && daysUntilExpiration > 5) {
-        // If between 6 and 10 days left
-        cout << "Your subscription will expire soon. Renew now!" << endl;
-    }
-
-    if (daysUntilExpiration > 10) {
-        // If more than 10 days left (i.e., 11)
+    else {
         cout << "You have an active subscription." << endl;
     }
 
     return 0;
 }
+
+
+
+
+    
+
+    
